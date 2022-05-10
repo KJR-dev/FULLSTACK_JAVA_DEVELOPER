9203560cbd0d5766641fa3d89af89858aa904ba0
JITENDRA SAHOO <sahooj168@gmail.com>
2022-05-11 01:57:34 +0530

    11/05/2022

--- Calculator/function.js
+++
@@ -1,5 +0,0 @@
-function b7() {
-    let x= document.getElementById(b7).Value;
-    let total=x;
-    show.console.log(total).innerHTML;
-}
---
+++ FRONTEND/Calculator/function.js
@@ -0,0 +1,4 @@
+function b7() {
+  let x = document.getElementById(b7()).Value;
+  console.log(x);
+}
